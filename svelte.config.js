fbf77c3125d0f429b5b8f1861b0d9c3b1ff833fc
Nick Baker <ndbaker1@outlook.com>
2021-08-07 16:33:20 -0500

    set favicon to be self resolved

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
 import preprocess from "svelte-preprocess";
 import staticAdapter from '@sveltejs/adapter-static'
 
+const base = process.env.BASE_HREF ? ('/' + process.env.BASE_HREF) : ''
+
 const config = {
   preprocess: [preprocess({
     "postcss": true
   })],
   kit: {
+    adapter: staticAdapter(),
+    paths: {
+      base,
+      assets: base
+    },
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    paths: {
-      base: process.env.BASE_HREF ? ('/' + process.env.BASE_HREF) : ''
-    },
-    adapter: staticAdapter(),
   }
 };
 
-export default config;
+export default config
