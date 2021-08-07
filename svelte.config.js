3821a6c3dff41ae737abf524c8f3b36dc7383956
Nick Baker <ndbaker1@outlook.com>
2021-08-07 14:30:55 -0500

    add sveltekit static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
 import preprocess from "svelte-preprocess";
+import staticAdapter from '@sveltejs/adapter-static'
 
 const config = {
   preprocess: [preprocess({
     "postcss": true
-  })]
+  })],
+  kit: {
+    adapter: staticAdapter(),
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte'
+  }
 };
 
 export default config;
