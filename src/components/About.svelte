74ab3da2c4b07f9df5f0f6dc48451cbf62f9c493
Nick Baker <ndbaker1@outlook.com>
2021-08-08 17:50:10 -0500

    added responsive styles and templated more html

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,56 +1,59 @@
 <script lang="ts">
   import { Links, Skills, Interests } from '../mydata'
+
+  const SkillsGroups = [
+    {
+      title: 'Languages',
+      chips: Skills.Languages,
+    },
+    {
+      title: 'Frameworks',
+      chips: [...Skills.FrontendFrameworks, ...Skills.BackendFrameworks],
+    },
+    {
+      title: 'Tools & Providers',
+      chips: Skills.ToolsAndProviders,
+    },
+    {
+      title: 'Areas of Interest',
+      chips: Interests,
+    },
+  ]
+
+  const LinkGroup = [
+    { title: 'GITHUB', url: Links.github },
+    { title: 'RESUME', url: Links.resume },
+  ]
 </script>
 
-<section id="about" class="themed h-screen flex align-center justify-center">
-  <div class="flex flex-col justify-center">
-    <div class="grid gap-5">
+<section id="about" class="themed min-h-screen flex align-center justify-center">
+  <div class="flex flex-col m-auto pt-10">
+    <div class="grid gap-2">
       <div>
-        <h2 class="text-3xl my-4">About Me</h2>
-        <p class="text-xl">
+        <h2 class="text-3xl my-2">About Me</h2>
+        <p class="md:text-xl px-10">
           Pursuing a Computer Science Degree at the <b>University of Texas at Dallas</b>
         </p>
       </div>
       <div>
-        <h2 class="text-3xl my-4">Skills / Interests</h2>
-        <div class="grid gap-2 text-left">
-          <div class="grid grid-cols-2">
-            <h2 class="skill-header">Languages</h2>
+        <h2 class="text-3xl my-2">Skills / Interests</h2>
+        <div class="grid md:grid-cols-2 md:gap-2 justify-center md:justify-left md:text-left">
+          {#each SkillsGroups as group}
+            <h2 class="skill-header">{group.title}</h2>
             <div>
-              {Skills.Languages.join(', ')}
+              {group.chips.join(', ')}
             </div>
-          </div>
-          <div class="grid grid-cols-2">
-            <h2 class="skill-header">Frameworks</h2>
-            <div>
-              {[...Skills.FrontendFrameworks, ...Skills.BackendFrameworks].join(', ')}
-            </div>
-          </div>
-          <div class="grid grid-cols-2">
-            <h2 class="skill-header">Tools & Providers</h2>
-            <div>
-              {Skills.ToolsAndProviders.join(', ')}
-            </div>
-          </div>
-          <div class="grid grid-cols-2">
-            <h2 class="skill-header">Areas of Interest</h2>
-            <div>
-              {Interests.join(', ')}
-            </div>
-          </div>
+          {/each}
         </div>
       </div>
-      <div class="grid grid-cols-2 gap-5">
-        <a href={Links.github} target="_blank" rel="noopener noreferrer">
-          <button class="button themed w-full rounded-full py-3 px-6 shadow-md hover:shadow-lg">
-            GITHUB
-          </button>
-        </a>
-        <a href={Links.resume} target="_blank" rel="noopener noreferrer">
-          <button class="button themed w-full rounded-full py-3 px-6 shadow-md hover:shadow-lg">
-            RESUME
-          </button>
-        </a>
+      <div class="flex flex-row justify-center space-x-6 mt-5">
+        {#each LinkGroup as link}
+          <a href={link.url} class="about-links" target="_blank" rel="noopener noreferrer">
+            <button class="button themed w-full rounded-full py-3 px-6 shadow-md hover:shadow-lg">
+              {link.title}
+            </button>
+          </a>
+        {/each}
       </div>
     </div>
   </div>
@@ -64,6 +67,9 @@
     font-weight: 600;
   }
   .skill-header + * {
-    max-width: 250px;
+    max-width: 300px;
+  }
+  .about-links {
+    min-width: 150px;
   }
 </style>
