49efd14945008d2323141ae5d8d5d8dc8bc1a7a6
Nick Baker <ndbaker1@outlook.com>
2021-09-22 20:03:09 -0500

    remove assets for build

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,6 @@ const config = {
     adapter: staticAdapter(),
     paths: {
       base,
-      assets: base
     },
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
