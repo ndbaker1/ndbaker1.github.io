10d478b5399712e12e5b5427ae3192c76643338a
Nick Baker <ndbaker1@outlook.com>
2022-08-19 04:56:28 -0500

    Refactor components to assemble resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,72 @@
 import { Component } from 'solid-js'
 import './App.scss'
-import { Resume } from './Resume'
+
+import { Competition, Education, Info, Section, SkillsSection, Work } from './Resume'
 
 const App: Component = () => {
   return (
-    <Resume />
+    <div id='resume'>
+      <div id="header">
+        <Info />
+      </div>
+      <div id="side">
+        <Section title='education'>
+          <Education
+            title="M.S. in Cyber Security"
+            location='University of Texas at Dallas'
+            date='August 2022 - Present' />
+
+          <Education
+            title="B.S. in Computer Science"
+            location='University of Texas at Dallas'
+            date='August 2019 - May 2022' />
+        </Section>
+
+        <SkillsSection />
+      </div>
+      <div id="main">
+        <Section title='work experience'>
+          <Work
+            position='AWS Software Development Engineer Intern'
+            company='AWS SageMaker'
+            location='Seattle, Washington'
+            date='May - August 2022'
+            items={['AWS SageMaker']} />
+
+          <Work
+            position='Software Developer'
+            company='Tritech Software Development'
+            date='June - March 2020'
+            location='Allen, TX'
+            items={[
+              `Contributed towards replatforming a monolithic Java product to a Cloud Provider and refactoring it into REST microservices using Kotlin, Spring, Hibernate, and PostgreSQL`,
+              `Supervised Auth0 integrations and configured several extensions to the authentication pipelines`,
+              `Provisioned and maintained cloud environments using Terraform and created Jenkins CI/CD jobs to orchestrate builds, dockerization, artifact staging, and deployments`,
+              `Led the process of bringing frontend (Angular) development in-house from a consulting partner`,
+              `Set up end-to-end testing pipeline through Cypress and Jenkins CI/CD`,
+              `Updated ETL Batch jobs aggregating data for Apache Superset`
+            ]} />
+        </Section>
+
+        <Section title='competitions'>
+          <Competition
+            event='HackUTD VI: News Unchained'
+            dateAndPlace='November 2019 | Richardson, TX'
+            items={[
+              `Chrome extension to scan online articles and identify bias using Google Cloud Natural Language API`,
+              `1st place winner for Best Text Objectivity Analysis (sponsored by Cognizant)`,
+            ]} />
+
+          <Competition
+            event='HackUTD VII: Lend-A-Hand'
+            dateAndPlace='February 2021 | Richardson, TX'
+            items={[
+              `Web App that provides a forum for sharing/lending tools to those in your community`,
+              `2st place winner for Texas Outage Recovery Solution (sponsored by StateFarm)`,
+            ]} />
+        </Section>
+      </div>
+    </div>
   )
 }
 
--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,25 +1,8 @@
-import { Component, For } from 'solid-js'
+import { Component, For, ParentProps } from 'solid-js'
 import './Resume.scss'
 
-export const Resume = () => {
-  return (
-    <div id='resume'>
-      <div id="header">
-        <Info />
-      </div>
-      <div id="side">
-        <EducationSection />
-        <SkillsSection />
-      </div>
-      <div id="main">
-        <WorkSection />
-        <CompetitionSection />
-      </div>
-    </div>
-  )
-}
 
-const Info: Component = () => {
+export const Info: Component = () => {
   return (
     <>
       <h2>Nicholas Baker</h2>
@@ -37,43 +20,7 @@ const Info: Component = () => {
   )
 }
 
-const EducationSection: Component = () => {
-  const Education: Component<{
-    title: string
-    location: string
-    date: string
-  }> = (props) => {
-    return (
-      <div>
-        <h4> {props.title} </h4>
-        <p> {props.location} </p>
-        <p>
-          <img src="https://img.icons8.com/material-outlined/192/calendar--v1.png" width="20px" />
-          {props.date}
-        </p>
-      </div>
-    )
-  }
-  return (
-    <div id="education">
-      <h2 class="section-title">education</h2>
-      <hr />
-
-      <Education
-        title="M.S. in Cyber Security"
-        location='University of Texas at Dallas'
-        date='August 2022 - Present' />
-
-      <Education
-        title="B.S. in Computer Science"
-        location='University of Texas at Dallas'
-        date='August 2019 - May 2022' />
-
-    </div>
-  )
-}
-
-const SkillsSection: Component = () => {
+export const SkillsSection: Component = () => {
   return (
     <div id="skills">
       <h2 class="section-title">skills</h2>
@@ -93,95 +40,78 @@ const SkillsSection: Component = () => {
   )
 }
 
-const WorkSection: Component = () => {
-  const Work: Component<{
-    position: string
-    location: string
-    company: string
-    date: string
-    items: string[]
-  }> = (props) => {
-    return (
-      <div class="summary">
-        <h4>{props.position}</h4>
-        <p>{props.company} <br /> {props.date} | {props.location} </p>
+type EducationProps = {
+  title: string
+  location: string
+  date: string
+}
 
-        <ul>
-          <For each={props.items}>
-            {note => <li> <p> {note} </p> </li>}
-          </For>
-        </ul>
-      </div>
-    )
-  }
-
+export const Education: Component<EducationProps> = (props) => {
   return (
     <div>
-      <h2 class="section-title">work experience</h2>
-      <hr />
-      <Work
-        position='Software Development Engineer Intern'
-        company='Amazon Web Services'
-        location='Seattle, Washington'
-        date='May 2022 - Present'
-        items={['AWS SageMaker']} />
-
-      <Work
-        position='Software Developer'
-        company='Tritech Software Development'
-        date='June 2020 - March 2020'
-        location='Allen, TX'
-        items={[
-          `Contributed towards replatforming a monolithic Java product to a Cloud Provider and refactoring it into REST microservices using Kotlin, Spring, Hibernate, and PostgreSQL`,
-          `Supervised Auth0 integrations and configured several extensions to the authentication pipelines`,
-          `Provisioned and maintained cloud environments using Terraform and created Jenkins CI/CD jobs to orchestrate builds, dockerization, artifact staging, and deployments`,
-          `Led the process of bringing frontend (Angular) development in-house from a consulting partner`,
-          `Set up end-to-end testing pipeline through Cypress and Jenkins CI/CD`,
-          `Updated ETL Batch jobs aggregating data for Apache Superset`
-        ]} />
+      <h4> {props.title} </h4>
+      <p> {props.location} </p>
+      <p>
+        <img src="https://img.icons8.com/material-outlined/192/calendar--v1.png" width="20px" />
+        {props.date}
+      </p>
     </div>
   )
 }
 
-const CompetitionSection: Component = () => {
+type WorkProps = {
+  position: string
+  location: string
+  company: string
+  date: string
+  items: string[]
+}
+
+export const Work: Component<WorkProps> = (props) => {
   return (
-    <>
-      <h2 class="section-title">competitions</h2>
+    <div class="summary">
+      <h4>{props.position}</h4>
+      <p>{props.company} <br /> {props.date} | {props.location} </p>
+
+      <ul>
+        <For each={props.items}>
+          {note => <li> <p> {note} </p> </li>}
+        </For>
+      </ul>
+    </div>
+  )
+}
+
+type SectionProps = ParentProps & {
+  title: string
+}
+
+export const Section: Component<SectionProps> = (props) => {
+  return (
+    <div>
+      <h2 class="section-title">{props.title}</h2>
       <hr />
+      {props.children}
+    </div>
+  )
+}
 
-      <div class="summary">
-        <h4> HackUTD VII: Lend-A-Hand </h4>
-        <p> February 2021 | Richardson, TX </p>
-        <ul>
-          <li>
-            <p>
-              Web App that provides a forum for sharing/lending tools to those in your community
-            </p>
-          </li>
-          <li>
-            <p>
-              2st place winner for Texas Outage Recovery Solution (sponsored by StateFarm)
-            </p>
-          </li>
-        </ul>
-      </div>
+type CompetitionProps = {
+  event: string
+  dateAndPlace: string
+  items: string[]
+}
 
-      <div class="summary">
-        <h4> HackUTD VI: News Unchained </h4>
-        <p> November 2019 | Richardson, TX </p>
-        <ul>
-          <li>
-            <p>
-              Chrome extension to scan online articles and identify bias using Google Cloud Natural Language API
-            </p>
-          </li>
-          <li>
-            <p>
-              1st place winner for Best Text Objectivity Analysis (sponsored by Cognizant)
-            </p>
-          </li>
-        </ul>
-      </div>
-    </>
+export const Competition: Component<CompetitionProps> = (props) => {
+  return (
+    <div class="summary">
+      <h4> {props.event} </h4>
+      <p> {props.dateAndPlace} </p>
+      <ul>
+        <For each={props.items}>
+          {item => <li> <p> {item} </p> </li>}
+        </For>
+      </ul>
+    </div>
   )
 }
