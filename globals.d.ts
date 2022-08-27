459865ca18a2953790fd69f6fb50d3d3a938ed92
Nick Baker <ndbaker1@outlook.com>
2022-08-27 16:18:50 -0500

    change type def location

--- globals.d.ts
+++ globals.d.ts
@@ -1,37 +1,37 @@
 declare module '*.yaml' {
+    export interface Portfolio {
+        education: Education[];
+        skills: Skills;
+        work: Work[];
+        competitions: Competition[];
+    }
+
+    export interface Competition {
+        event: string;
+        date: string;
+        location: string;
+        items: string[];
+    }
+
+    export interface Education {
+        title: string;
+        school: string;
+        date: string;
+    }
+
+    export interface Skills {
+        tools: string[];
+        languages: string[];
+    }
+
+    export interface Work {
+        company: string;
+        position: string;
+        location: string;
+        date: string;
+        items: string[];
+    }
+
     const data: Portfolio;
     export default data;
-}
-
-export interface Portfolio {
-    education: Education[];
-    skills: Skills;
-    work: Work[];
-    competitions: Competition[];
-}
-
-export interface Competition {
-    event: string;
-    date: string;
-    location: string;
-    items: string[];
-}
-
-export interface Education {
-    title: string;
-    school: string;
-    date: string;
-}
-
-export interface Skills {
-    tools: string[];
-    languages: string[];
-}
-
-export interface Work {
-    company: string;
-    position: string;
-    location: string;
-    date: string;
-    items: string[];
 }
